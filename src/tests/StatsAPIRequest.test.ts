cdc976408fa3e24b472d83b515f7a000acf72c7a
David Battefeld <davidbattefeld@gmail.com>
2024-03-24 18:53:44 +0100

    test: add test for StatsAPIRequest

---
+++ src/tests/StatsAPIRequest.test.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import {StatsAPIRequest} from "../service/StatsAPIRequest.js";
+import {StatsType} from "../enum/StatsType.js";
+
+describe("Stats API Request", () => {
+    const request = new StatsAPIRequest(process.env.TEST_API_KEY!)
+
+    test("Single Player - Pitching", async () => {
+        const personID = 76222 // that's me!
+        const season = 2023
+        const statsType = StatsType.pitching
+
+        const result = await request.getStatisticsForPerson(personID, statsType, season)
+        expect(result).toHaveProperty("data")
+        expect(result).toHaveProperty("summaries")
+    })
+})
