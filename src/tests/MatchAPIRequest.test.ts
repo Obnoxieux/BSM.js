4f478eb492fba4a7ef9493afec4055289e9f38c0
David Battefeld <davidbattefeld@gmail.com>
2024-04-10 23:31:44 +0200

    test: add tests for match boxscore

--- src/tests/MatchAPIRequest.test.ts
+++ src/tests/MatchAPIRequest.test.ts
@@ -9,12 +9,37 @@ describe("Match API Request", () => {
         const clubID = 485
         const season = 2023
         const gameday = Gameday.previous
-
         const result = await request.loadGamesForClub(clubID, season, gameday)
+
         expect(Array.isArray(result)).toBe(true)
         expect(result).toBeTruthy()
         expect(result[0]).toHaveProperty("away_team_name")
         expect(result[0]).toHaveProperty("home_team_name")
         expect(result[0]).toHaveProperty("league")
+    })
+
+    test("Boxscore for single Match - exists", async () => {
+        const matchID = 47099
+        const result = await request.getBoxscoreForGame(matchID)
+
+        expect(result).toBeTruthy()
+        expect(result).toHaveProperty("header")
+        expect(result).toHaveProperty("linescore")
+        expect(result).toHaveProperty("offensive_away")
+        expect(result).toHaveProperty("additional_away")
+        expect(result).toHaveProperty("offensive_home")
+        expect(result).toHaveProperty("additional_home")
+        expect(result).toHaveProperty("pitching_away")
+        expect(result).toHaveProperty("pitching_home")
+        expect(result).toHaveProperty("game_notes")
+        expect(result).toHaveProperty("full_boxscore_html")
+    })
+
+    // TODO: this test will always fail right now because the function never completes due to HTML being sent back
+    test("Boxscore for single Match - does not exist", async () => {
+        const matchID = 0
+        const result = await request.getBoxscoreForGame(matchID)
+
+        expect(result).toBe(undefined)
     })
 })
