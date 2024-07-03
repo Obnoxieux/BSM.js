2b90991c9dbb40ef40df226628a69e2e530c1da6
David Battefeld <davidbattefeld@gmail.com>
2024-07-03 11:54:29 +0200

    feat: add season filter to club team load

--- src/service/ClubTeamsAPIRequest.ts
+++ src/service/ClubTeamsAPIRequest.ts
@@ -6,11 +6,17 @@ export class ClubTeamsAPIRequest extends AbstractAPIRequest {
      * Load all Club teams ("Mannschaften") for a single club.
      *
      * Scope: Club
+     *
      * @param clubID ID of club to load teams for
+     * @param season the season to load teams for, defaults to current
      * @throws ParseError
      * @throws FetchError
      */
-    public async getTeamsForClub(clubID: number): Promise<ClubTeam[] | undefined> {
-        return await this.apiCallGET<ClubTeam[]>(`clubs/${clubID}/team_clubs.json`, [])
+    public async getTeamsForClub(clubID: number, season?: number): Promise<ClubTeam[] | undefined> {
+        const queryParameters: string[][] = [
+            [this.SEASON_FILTER, season?.toString() ?? this.defaultSeason.toString()],
+        ]
+
+        return await this.apiCallGET<ClubTeam[]>(`clubs/${clubID}/team_clubs.json`, queryParameters)
     }
 }
