990ebd88ab0e9cfc3619e3203aea81eb3985e92f
David Battefeld <davidbattefeld@gmail.com>
2024-07-03 10:49:16 +0200

    feat: add single leagueGroup load func

--- src/service/LeagueGroupAPIRequest.ts
+++ src/service/LeagueGroupAPIRequest.ts
@@ -3,6 +3,15 @@ import {LeagueGroup} from "../model/LeagueGroup.js";
 
 export class LeagueGroupAPIRequest extends AbstractAPIRequest{
     /**
+     * Get a single LeagueGroup by ID.
+     *
+     * @param id the ID to get
+     */
+    public async getSingleLeagueGroup(id: number): Promise<LeagueGroup> {
+        return await this.apiCallGET<LeagueGroup>(`league_groups/${id}.json`, [])
+    }
+
+    /**
      * Get all LeagueGroups for a single Club. Unfortunately there is no separate endpoint, so the API key decides the club
      * for which league groups are loaded.
      *
@@ -12,7 +21,7 @@ export class LeagueGroupAPIRequest extends AbstractAPIRequest{
      * @throws ParseError
      * @throws FetchError
      */
-    async getLeagueGroupsForClub(season: number): Promise<LeagueGroup[]> {
+    public async getLeagueGroupsForClub(season: number): Promise<LeagueGroup[]> {
         const response = await this.apiCallGET<LeagueGroup[]>("league_groups.json",[
             [this.SEASON_FILTER, season.toString()]
         ])
