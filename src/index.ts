b47ace0f0f80312fcca8faeb5a0db07f182de1fd
David Battefeld <davidbattefeld@gmail.com>
2024-07-05 09:58:17 +0200

    build: test star export in index.ts

--- src/index.ts
+++ src/index.ts
@@ -26,3 +26,4 @@ export {PitchingStatisticsEntry} from "./model/PitchingStatisticsEntry.js";
 export {Table} from "./model/Table.js";
 export {Team} from "./model/Team.js";
 export {GameClassEntry} from "./model/GameClassEntry.js";
+export * from "./model/AbstractStatisticsEntry.js";
